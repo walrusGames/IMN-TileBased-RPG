80695c36508fa80ae102d33565782bddf3c5c99c
Alex Valcourt Caron <vala2004@usherbrooke.ca>
2016-04-26 22:28:06 -0400

    Le perso bug pu din murs

--- ProjetPOO2016/SFMLproject/Tiles/TileCharacter.cs
+++ ProjetPOO2016/SFMLproject/Tiles/TileCharacter.cs
@@ -70,7 +70,7 @@ namespace SFMLproject.Tiles
                 mapState.moveMapView(new Vector2f(move.X, move.Y) * Constants.tileSize);
                 mapState.setState(mapState);
             }
-            //else mapState.Queue(mapState.getTile(getPos())); 
+            else mapState.Queue(mapState.getTile(getPos())); 
         }
 
         public override void updateOnReact(Vector2i ind)
