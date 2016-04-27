7132baaf3e44149176dd933b1a275a72ecf76129
Léa Bélisle Tourigny <bell2912@usherbrooke.ca>
2016-04-27 14:01:06 -0400

    add invoker command abstract

---
+++ ProjetPOO2016/SFMLproject/Command/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLproject.Encounter_ENV
+{
+    interface Command
+    {
+        void execute();
+    }
+}
---
+++ ProjetPOO2016/SFMLproject/Menu/MenuElement.cs
@@ -0,0 +1,16 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLproject.Menu
+{
+    abstract class MenuElement
+    {
+        abstract public void draw(RenderWindow window);
+
+    }
+}
