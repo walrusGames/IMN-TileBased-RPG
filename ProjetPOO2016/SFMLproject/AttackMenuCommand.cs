2345cf363729ec8567e3db2e21373af9b030e5cf
Léa Bélisle Tourigny <bell2912@usherbrooke.ca>
2016-04-27 16:23:03 -0400

    add attack mechanism

---
+++ ProjetPOO2016/SFMLproject/AttackMenuCommand.cs
@@ -0,0 +1,29 @@
+﻿using SFMLproject.Menu;
+using SFMLproject.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLproject.Encounter_ENV
+{
+    class AttackMenuCommand : Command
+    {
+        private Encounter e;
+        private MenuEncounter m; 
+        public AttackMenuCommand(Encounter enc)
+        {
+            e = enc;
+        }
+
+        public AttackMenuCommand(MenuEncounter menu)
+        {
+            m = menu; 
+        }
+        public void execute()
+        {
+            e.attackSubMenu();
+        }
+    }
+}
---
+++ ProjetPOO2016/SFMLproject/Object/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFMLproject.Object
+{
+    class Attack
+    {
+        private float damage;
+        private String name;
+        private Dictionary<StaticFields.attackType, float> weaknesses;
+
+        public Attack(String n, float dmg)
+        {
+            name = n;
+            damage = dmg;
+        }
+
+        public float getDamage()
+        {
+            return damage; 
+        }
+
+        public String getName()
+        {
+            return name;
+
+        }
+    }
+}
