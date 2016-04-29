9337ac053d7ec6b48b4d78c2619ff5138844a0b7
David Lapointe <davidlapointe123@hotmail.com>
2016-04-29 12:44:04 -0400

    hu

---
+++ ProjetPOO2016/SFMLproject/Command/SaveCommand.cs
@@ -0,0 +1,41 @@
+﻿using SFML.System;
+using SFMLproject.Events;
+using SFMLproject.Menu;
+using SFMLproject.Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SFMLproject.Command
+{ 
+    class SaveCommand : Encounter_ENV.Command
+    {
+        Dialogue dia;
+        protected Character character;
+
+        public void execute()
+
+        {
+            throw new NotImplementedException();
+        }
+
+        public void execute(Character c)
+        {
+            List<Character.characDialogueStruc> tex = new List<Character.characDialogueStruc>();
+            Character.characDialogueStruc line = new Character.characDialogueStruc();
+            line.dialogue = "Game has been saved.";
+            line.id = 0;
+            line.nextIdList = new List<int>() { -1 };
+            tex.Add(line);
+
+            dia = new Dialogue(tex);
+            Vector2f position = new Vector2f(50, 350);
+            character.SaveCharacter();
+            dia.afficher(position);
+            
+        }
+    }
+}
